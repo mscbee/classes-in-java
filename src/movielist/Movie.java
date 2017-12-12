43d17614f0c8ab793975fad60212525d66c2280d
Charlene <charlenebadibanga@gmail.com>
2017-12-12 11:02:50 +0000

    added mutator and accessor methods (setters and getters)

--- src/movielist/Movie.java
+++ src/movielist/Movie.java
@@ -18,6 +18,40 @@ public class Movie {
         this.runtime = runtime;
     }
 
+    //accessor aka getter
+    public String getMovieName() {
+        return movieName;
+    }
+
+    //mutator aka setter
+    public void setMovieName(String value) {
+        movieName = value;
+    }
+
+    public char getRating() {
+        return rating;
+    }
+
+    public void setRating (char value) {
+        rating = value;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int value) {
+        year = value;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int value) {
+        runtime = value;
+    }
+
     //method to return information
     public String toString() {
         return " Movie: " + movieName +
